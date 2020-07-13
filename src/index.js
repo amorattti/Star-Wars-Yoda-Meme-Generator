16684088469acaeecc130b1682b81a72fe38d73d
tobiasz18 <tobiaszpost@gmail.com>
2020-07-13 20:34:01 +0200

    cosmetic changes

--- src/index.js
+++ src/index.js
@@ -15,11 +15,10 @@ import {
     const createImg = new CreateImages(DOMelements.build, DOMelements.backup); // just create imagines in containers
     const canvasApi = new CanvasApi(DOMelements.build, DOMelements.backup, DOMelements.canvas); // run canvas and use methods
     const cssApi = new CssApi(); // css api, hide or show item
-
+    
+    setLoader(cssApi);
     createImg.createBuildSection();
     createImg.createBackupSection();
-
-    setLoader(cssApi)
     setOnClickEvents(canvasApi, cssApi);
     setEventsListener(0);
     dnd(canvasApi);
